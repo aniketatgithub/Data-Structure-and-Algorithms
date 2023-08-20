6cb1ac03934a66aa5c3ae5cf989d5888f78c9995
Aniket <42694864+aniketatgithub@users.noreply.github.com>
2023-08-20 08:46:30 +0100

    Printing-SubArrays
    Find All the Subarrays of a Given Array in Java

---
+++ Printing-Subarrays.java
@@ -0,0 +1,16 @@
+//Find All the Subarrays of a Given Array in Java
+class Subarrays {
+    
+    public static void main(String[] args) {
+            int arr[] = {1,2,3,4,5,6,7};
+            for (int i = 0; i < arr.length; i++) {
+                for (int j = i+1; j < arr.length; j++) {
+                    for (int j2 = i; j2 < j; j2++) {
+                        System.out.print(" (" + arr[i] + " , " + arr[j2] + "),");
+                    }
+                      System.out.println("");
+                }
+                System.out.println("");
+            }
+    }
+}
