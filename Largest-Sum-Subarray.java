e744586bc9f2ea498a91331ec3270b675b0bb859
Aniket <42694864+aniketatgithub@users.noreply.github.com>
2023-08-24 19:36:24 -0700

    Added Maxium Array Sum using Vector

--- Largest-Sum-Subarray.java
+++ Largest-Sum-Subarray.java
@@ -1,25 +1,26 @@
 //Print the sum of each subarray & find out the largest sum
-// class LargestSum{
-//     public static void main(String[] args) {
-//         int max =0; int tempSum = 0;
-//          int arr[] = {1,2,3,4,5,6,7};
-//             for (int i = 0; i < arr.length; i++) {
-//                 tempSum = 0;
-//                 for (int j = i; j < arr.length; j++) {
-//                     for (int j2 = i; j2 <= j; j2++) {
-//                         System.out.print(" (" + arr[j2] + "),");
-//                         tempSum+= arr[j2];
-//                     }
-//                     System.out.println("Total sum : " + tempSum );
-//                       System.out.println("");
-//                     if(max < tempSum) max = tempSum;
-//                     tempSum = 0;
-//                 }
-//                 System.out.println("");
-//             }
-//              System.out.println("max is " + max);
-//     }
-// }
+//Comment off other classes to run seprately
+class LargestSum{
+    public static void main(String[] args) {
+        int max =0; int tempSum = 0;
+         int arr[] = {1,2,3,4,5,6,7};
+            for (int i = 0; i < arr.length; i++) {
+                tempSum = 0;
+                for (int j = i; j < arr.length; j++) {
+                    for (int j2 = i; j2 <= j; j2++) {
+                        System.out.print(" (" + arr[j2] + "),");
+                        tempSum+= arr[j2];
+                    }
+                    System.out.println("Total sum : " + tempSum );
+                      System.out.println("");
+                    if(max < tempSum) max = tempSum;
+                    tempSum = 0;
+                }
+                System.out.println("");
+            }
+             System.out.println("max is " + max);
+    }
+}
 
 
 //Way 2 : Prefix Sums
---
+++ MaximumSubarraySum.java
@@ -0,0 +1,42 @@
+//23.
+
+import java.util.Scanner;
+import java.util.Vector;
+
+public class MaximumSubarraySum {
+    public static void main(String[] args) {
+                Scanner scan = new Scanner(System.in);
+                System.out.println("Enter the size of vector: ");
+                int size = scan.nextInt();
+                System.out.println("Enter the elemets : ");
+                Vector<Integer> vector = new Vector<>();
+                for (int i = 0; i < size; i++) {
+                    vector.add(i, scan.nextInt());
+               }
+            //    for (int i = 0; i < vector.size(); i++) {
+            //         System.out.println(vector.get(i));
+            //    }
+                int maxElement = Integer.MIN_VALUE;
+                Boolean isAllnegative = true;
+                for (int i = 0; i < vector.size(); i++) {
+                    if(vector.get(i)>0){
+                        isAllnegative = false;
+                    }
+                    if(maxElement<vector.get(i)){
+                        maxElement = vector.get(i);
+                    }
+                }
+               if(isAllnegative){
+               System.out.println("Allnegative is true " + maxElement) ;
+            }
+            int max = 0; int curr = 0 ;
+               for (int i = 0; i < vector.size(); i++) {
+                    curr = curr + vector.get(i); 
+                    if(curr<0){ curr = 0;  }
+                    if(curr > max) {  max = curr;}
+               }
+
+ System.out.println(max) ;
+
+    }
+}
